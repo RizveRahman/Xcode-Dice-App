a127bded3e8b89fc9ced6b89ab19aa9ddf5081a1
Rizve Rahman <rizverahman.dev@gmail.com>
2023-01-23 23:27:30 +0600

    - Now it's fully responsive

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImg1: UIImageView!
+    @IBOutlet weak var diceImg2: UIImageView!
+    
+    //var leftDiceCount = 0
+    //var rightDiceCount = 5
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollDiceButton(_ sender: UIButton) {
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix"),]
+        
+        diceImg1.image = diceArray[Int.random(in: 0...5)]
+        diceImg2.image = diceArray.randomElement()
+        
+        //leftDiceCount += 1
+        //rightDiceCount -= 1
+        
     }
-
-
+    
 }
 
